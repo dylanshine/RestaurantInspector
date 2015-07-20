a0631d76abe23e1c44f49c86360597d477ea3a5b
Dylan Shine <dylanshinenyc@gmail.com>
2015-07-20 14:15:29 -0400

    Add user current location to map

--- RestaurantInsepctor/RestaurantInsepctor/ViewController.m
+++ RestaurantInsepctor/RestaurantInsepctor/ViewController.m
@@ -8,22 +8,74 @@
 
 #import "ViewController.h"
 #import <MapKit/MapKit.h>
+#import <INTULocationManager.h>
 
-@interface ViewController ()
+@interface ViewController () <MKMapViewDelegate>
+@property (assign, nonatomic) INTULocationRequestID locationRequestID;
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
-
+@property (nonatomic) CLLocation *currentLocation;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.mapView.delegate = self;
+    [self.mapView setShowsUserLocation:YES];
+    [self startLocationUpdateSubscription];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - UNTULocationManager
+
+- (void)startLocationUpdateSubscription {
+    __weak __typeof(self) weakSelf = self;
+    INTULocationManager *locMgr = [INTULocationManager sharedInstance];
+    self.locationRequestID = [locMgr subscribeToLocationUpdatesWithBlock:^(CLLocation *currentLocation, INTULocationAccuracy achievedAccuracy, INTULocationStatus status) {
+        __typeof(weakSelf) strongSelf = weakSelf;
+        
+        if (status == INTULocationStatusSuccess) {
+            // A new updated location is available in currentLocation, and achievedAccuracy indicates how accurate this particular location is
+            strongSelf.currentLocation = currentLocation;
+            
+            [self centerMapOnLocation:currentLocation];
+        }
+        else {
+            // An error occurred, which causes the subscription to cancel automatically (this block will not execute again unless it is used to start a new subscription).
+            strongSelf.locationRequestID = NSNotFound;
+            NSLog(@"%@", [strongSelf getErrorDescription:status]);
+        }
+    }];
+}
+
+- (NSString *)getErrorDescription:(INTULocationStatus)status {
+    if (status == INTULocationStatusServicesNotDetermined) {
+        return @"Error: User has not responded to the permissions alert.";
+    }
+    if (status == INTULocationStatusServicesDenied) {
+        return @"Error: User has denied this app permissions to access device location.";
+    }
+    if (status == INTULocationStatusServicesRestricted) {
+        return @"Error: User is restricted from using location services by a usage policy.";
+    }
+    if (status == INTULocationStatusServicesDisabled) {
+        return @"Error: Location services are turned off for all apps on this device.";
+    }
+    return @"An unknown error occurred.\n(Are you using iOS Simulator with location set to 'None'?)";
+}
+
+#pragma mark - MapKit
+
+-(void) centerMapOnLocation:(CLLocation *)location {
+    
+    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(location.coordinate, 2000.0, 2000.0);
+    
+    [self.mapView setRegion:region];
+    
 }
 
 @end
