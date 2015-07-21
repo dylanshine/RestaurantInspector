3c63c9ae30393cefe2f20746fe74d4ebc5bcebe1
Dylan Shine <dylanshinenyc@gmail.com>
2015-07-21 17:03:32 -0400

    Add multipage api call with AFDatastore delegate

--- RestaurantInsepctor/RestaurantInsepctor/AFDataStore.h
+++ RestaurantInsepctor/RestaurantInsepctor/AFDataStore.h
@@ -9,10 +9,24 @@
 #import <Foundation/Foundation.h>
 #import <CoreLocation/CoreLocation.h>
 
+
+@class AFDataStore;
+
+@protocol AFDataStoreDelegate <NSObject>
+
+@required
+-(void)dataStore:(AFDataStore *)dataStore didLoadRestaurants:(NSArray *)restaurants;
+
+@end
+
+
 @interface AFDataStore : NSObject
+
 @property (nonatomic) NSMutableArray *results;
+@property (nonatomic, weak) id<AFDataStoreDelegate> delegate;
+
 +(instancetype)sharedData;
--(void)getRestaurantsWith:(NSInteger)radius CurrentLocation:(CLLocation *)currentLocation Completion:(void (^)())completion;
+-(void)getRestaurantsWith:(NSInteger)radius CurrentLocation:(CLLocation *)currentLocation; // Completion:(void (^)())completion;
 -(void)getDetailsForRestaurantID:(NSString *)placeID Completion:(void (^)(NSString *))completion;
 -(void)getRestaurantInfoWithCompletion:(NSString *)phoneNumber completionBlock:(void(^)(NSArray *))completionBlock;
 
