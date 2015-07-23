47fdc3c6970123f5cfa81b73115245f90f76efeb
Dylan Shine <dylanshinenyc@gmail.com>
2015-07-23 18:13:30 -0400

    Make status bar hidden on modal VC

--- RestaurantInsepctor/RestaurantInsepctor/RestaurantDetailViewController.m
+++ RestaurantInsepctor/RestaurantInsepctor/RestaurantDetailViewController.m
@@ -28,14 +28,8 @@
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+-(BOOL) prefersStatusBarHidden {
+    return YES;
 }
-*/
 
 @end
