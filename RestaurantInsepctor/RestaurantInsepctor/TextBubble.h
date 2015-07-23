ce818f309d603143ca73fee2ae9fd672dee75dce
Dylan Shine <dylanshinenyc@gmail.com>
2015-07-23 13:25:15 -0400

    Add Masonry to project

---
+++ RestaurantInsepctor/RestaurantInsepctor/TextBubble.h
@@ -0,0 +1,13 @@
+//
+//  TextBubble.h
+//  RestaurantInsepctor
+//
+//  Created by Dylan Shine on 7/23/15.
+//  Copyright (c) 2015 Dylan Shine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TextBubble : UIView
+
+@end
---
+++ RestaurantInsepctor/RestaurantInsepctor/TextBubble.m
@@ -0,0 +1,21 @@
+//
+//  TextBubble.m
+//  RestaurantInsepctor
+//
+//  Created by Dylan Shine on 7/23/15.
+//  Copyright (c) 2015 Dylan Shine. All rights reserved.
+//
+
+#import "TextBubble.h"
+
+@implementation TextBubble
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
