bcbf2933c7727e326a9571176d48f80e6e0d8a63
Dylan Shine <dylanshinenyc@gmail.com>
2015-07-31 20:27:32 -0400

    Add Crashlytics to project

--- RestaurantInsepctor/Pods/Target Support Files/Pods/Pods-environment.h
+++ RestaurantInsepctor/Pods/Target Support Files/Pods/Pods-environment.h
@@ -54,6 +54,24 @@
 #define COCOAPODS_VERSION_MINOR_Asterism 0
 #define COCOAPODS_VERSION_PATCH_Asterism 0
 
+// Crashlytics
+#define COCOAPODS_POD_AVAILABLE_Crashlytics
+#define COCOAPODS_VERSION_MAJOR_Crashlytics 3
+#define COCOAPODS_VERSION_MINOR_Crashlytics 1
+#define COCOAPODS_VERSION_PATCH_Crashlytics 0
+
+// Fabric
+#define COCOAPODS_POD_AVAILABLE_Fabric
+#define COCOAPODS_VERSION_MAJOR_Fabric 1
+#define COCOAPODS_VERSION_MINOR_Fabric 2
+#define COCOAPODS_VERSION_PATCH_Fabric 8
+
+// Fabric/Base
+#define COCOAPODS_POD_AVAILABLE_Fabric_Base
+#define COCOAPODS_VERSION_MAJOR_Fabric_Base 1
+#define COCOAPODS_VERSION_MINOR_Fabric_Base 2
+#define COCOAPODS_VERSION_PATCH_Fabric_Base 8
+
 // FontAwesomeKit
 #define COCOAPODS_POD_AVAILABLE_FontAwesomeKit
 #define COCOAPODS_VERSION_MAJOR_FontAwesomeKit 2
